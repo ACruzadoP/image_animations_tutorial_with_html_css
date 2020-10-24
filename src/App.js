f987efbbbfd5485bde776281346e68250e5895f8
Alberto <albertocruzadopleite@gmail.com>
2020-10-24 16:21:55 +0200

    The exercise was successfully completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      HOLA MUNDO
-    </div>
+    <React.Fragment>
+        <div className="title">
+          Image Animations
+        </div>
+
+        <div className="images">
+          <div className="image-container">
+            <img src="/img/pic1.jpg" alt="" />
+            <img className="transition-image" src="/img/pic2.jpg" alt="" />
+          </div>
+
+          <div className="image-container fade">
+            <img src="/img/pic3.jpg" alt="" />
+          </div>
+        </div>
+        
+    </React.Fragment>
+    
   );
 }
 
